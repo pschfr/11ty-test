841320f3572cde8c255e79df6e567bbd69a3c005
Paul Schaefer <yeppaul2@gmail.com>
2018-12-29 17:40:07 -0500

    Added simple filter using toLocaleDateString

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,14 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addCollection("posts", function(collection) {
 		return collection.getFilteredByGlob("_posts/*.*");
 	});
+
+	// Simple date filter using toLocaleDateString
+	eleventyConfig.addFilter("prettyDate", function(value) {
+		return new Date(value).toLocaleDateString("en-US", {
+			timeZone: "UTC",
+			month: "long",
+			day: "numeric",
+			year: "numeric"
+		});
+	});
 };
