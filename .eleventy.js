0bf7e0bb348f5f1a5b3869673bd611edb925fa63
Paul Schaefer <yeppaul2@gmail.com>
2018-12-29 11:19:16 -0500

    Disabled pathPrefix setting for now

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
 // This file overrides any Eleventy settings we needed to change.
 module.exports = function() {
 	return {
-		// Changes deployment subdirectory for GitHub Pages
-		// https://www.11ty.io/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
-		pathPrefix: "11ty-test"
 	}
 };
