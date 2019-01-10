5c990a9c5993d4aa12dc806536287a086ffc3956
Paul Schaefer <yeppaul2@gmail.com>
2019-01-09 20:39:41 -0500

    Moved minifier filters into their own folder.

--- .eleventy.js
+++ .eleventy.js
@@ -9,11 +9,11 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addFilter("prettyDate", require("./_filters/prettyDate.js"));
 
 	// CSS minification filter powered by clean-css
-	eleventyConfig.addFilter("cssMin", require("./_filters/cssMin.js"));
+	eleventyConfig.addFilter("cssMin", require("./_filters/minifiers/css.js"));
 
 	// JS minification filter powered by uglify-js
-	eleventyConfig.addFilter("jsMin", require("./_filters/jsMin.js"));
+	eleventyConfig.addFilter("jsMin", require("./_filters/minifiers/js.js"));
 
 	// HTML minification transform powered by html-minifier
-	eleventyConfig.addTransform("htmlMin", require("./_filters/htmlMin.js"));
+	eleventyConfig.addTransform("htmlMin", require("./_filters/minifiers/html.js"));
 };
